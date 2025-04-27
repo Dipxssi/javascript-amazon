9dfb8b4a1e23273bf494c6dcc15d9d12137e8f37
DIPSHA <dipshadas4@gmail.com>
2025-04-27 12:04:07 +0530

    Save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,11 +1,21 @@
-export let cart = [{
-   productId : //De-duplicating data or normalizing
-   'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-   quantity: 2,
-},{
-  productId : '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1
-}];
+export let cart =JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+    cart = [{
+      productId : //De-duplicating data or normalizing
+      'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 2,
+   },{
+     productId : '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+     quantity: 1
+   }];
+}
+
+
+
+function saveToStorage() {
+    localStorage.setItem('cart',JSON.stringify(cart));
+}
 
 export function addToCart(productId, selectorQuantity) {
   let matchingItem;
@@ -23,6 +33,7 @@ export function addToCart(productId, selectorQuantity) {
      quantity : selectorQuantity
     });
   }
+  saveToStorage();
 }
 
 
@@ -34,5 +45,6 @@ export function removefromCart(productId) {
       newCart.push(cartItem);
     }
   });
-  cart = newCart
+  cart = newCart;
+  saveToStorage();
 }
