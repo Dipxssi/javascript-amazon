c4a103ce2186d14fce0f99f2b5edec876766c1ad
DIPSHA <dipshadas4@gmail.com>
2025-05-04 12:13:11 +0530

    load products from the backend

--- data/products.js
+++ data/products.js
@@ -59,7 +59,27 @@ const date = new Date();
 console.log(date);
 console.log(date.toLocaleDateString());
 */
+export let products = [];
 
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load' , () => {
+     products = JSON.parse(xhr.response).map((productDetails) =>{
+      if (productDetails.type === 'clothing'){
+       return new Clothing(productDetails);
+      }
+      return  new Product(productDetails);
+   }); //to convert json into obj
+   console.log('load products');
+   fun();
+  });
+  xhr.open('GET','https://supersimplebackend.dev/products');
+  xhr.send();
+}
+
+
+/*
  export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -724,4 +744,4 @@ console.log(date.toLocaleDateString());
     return new Clothing(productDetails);
    }
    return  new Product(productDetails);
-});
+});*/
